2feec137bd51dc93d517feaca9550e81a5634939
romansahel <roman.sahel1987@gmail.com>
2022-12-19 12:51:52 -0800

    The Basc Structure of the Solar System

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This Basic Structure us Ready Please write down you code in this project");
+    }
+}
